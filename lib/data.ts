4447a0b533a45b504fdf68288e1c99b5d18400f8
Priyanshu sharma <168932531+Valtriac42@users.noreply.github.com>
2025-11-16 22:49:04 +0530

    final updation

--- lib/data.ts
+++ lib/data.ts
@@ -1,4 +1,4 @@
-import { FaCertificate, FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import { FaCertificate, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 import { MdMail } from "react-icons/md";
 
 export const experienceData = [
